3564f4ffc4be2d4e12e0967cbb1adb9619921577
carlos-augusto-roque <carlos@ofsolution.com>
2022-02-01 12:27:36 -0300

    .

---
+++ my-first-web-api/my-first-web-api/src/main/java/dio/web/handler/CampoObrigatorioException.java
@@ -0,0 +1,10 @@
+package dio.web.handler;
+
+public class CampoObrigatorioException  extends BusinessException{
+
+	public CampoObrigatorioException(String campo) {
+		super("O campo %s é obrigatório", campo);
+		
+	}
+	
+}
